0fb8e8e5e6ff82b1b29e67747ed77351d032c37e
Jonathan <96206878+belicfr@users.noreply.github.com>
2023-12-13 16:55:13 +0100

    Ajout en secondaire des couleurs du projet

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,15 @@ module.exports = {
     "./*.html",
   ],
   theme: {
-    colors: {
-      "orange": "#FF7A00",
-      "orange-light": "#FFCEA1",
-      "white": "#FFFFFF",
-      "blue": "#0094FF",
-      "grey": "#686868",
-      "grey-light": "#E6E6E6",
+    extend: {
+      colors: {
+        "orange": "#FF7A00",
+        "orange-light": "#FFCEA1",
+        "white": "#FFFFFF",
+        "blue": "#0094FF",
+        "grey": "#686868",
+        "grey-light": "#E6E6E6",
+      }
     },
   },
   plugins: [
