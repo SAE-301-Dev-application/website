12a3476b5d4ea93bdcb1929adfd03eda227e6c5f
Simon Guiraud <78082685+SyberSim@users.noreply.github.com>
2023-12-20 11:49:55 +0100

    components pour le header et le footer

---
+++ src/Views/Components/FooterComponent.php
@@ -0,0 +1,9 @@
+<footer class="grey-footer">
+    <h1 class="logo"></h1>
+
+    <div class="copyrights">
+    2023 © Festiplan. IUT de Rodez.
+    </div>
+
+    <h1 class="logo"></h1>
+</footer>
---
+++ src/Views/Components/HeaderComponent.php
@@ -0,0 +1,45 @@
+<header>
+    <nav>
+    <h1 class="logo"></h1>
+
+    <div class="links">
+        <a href="dashboard.html" class="active-link">
+        <i class="fa-solid fa-home"></i>
+        Tableau de bord
+        </a>
+
+        <a href="festivals.html">
+        <i class="fa-solid fa-champagne-glasses"></i>
+        Festivals
+        </a>
+
+        <a href="#">
+        <i class="fa-solid fa-masks-theater"></i>
+        Spectacles
+        </a>
+    </div>
+
+    <div class="user-menu">
+        <div class="user-name">
+        <p>
+            <i class="fa-solid fa-angle-down"></i>
+            Utilisateur
+        </p>
+        </div>
+
+        <div class="menu-dropdown-container">
+        <div class="menu-dropdown">
+            <a href="#">
+            <i class="fa-solid fa-user fa-fw"></i>
+            Mon profil
+            </a>
+
+            <a href="#">
+            <i class="fa-solid fa-arrow-right-from-bracket fa-fw"></i>
+            Déconnexion
+            </a>
+        </div>
+        </div>
+    </div>
+    </nav>
+</header>
