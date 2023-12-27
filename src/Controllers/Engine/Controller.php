306b6f21e89498a5e99b79056bbe790ab1b90665
Jonathan <96206878+belicfr@users.noreply.github.com>
2023-12-27 17:56:19 +0100

    PhpDoc
    Controller.php

--- src/Controllers/Engine/Controller.php
+++ src/Controllers/Engine/Controller.php
@@ -5,6 +5,16 @@ namespace MvcLite\Controllers\Engine;
 use MvcLite\Middlewares\Engine\Middleware;
 use MvcLite\Middlewares\ExistingAccountMiddleware;
 
+/**
+ * General controller class. Used by all MVCLite
+ * controllers.
+ *
+ * It implements controller utilities methods like
+ * middleware one. Also, it adds global controller
+ * scripts like ExistingAccount middleware call.
+ *
+ * @author belicfr
+ */
 class Controller
 {
     public function __construct()
