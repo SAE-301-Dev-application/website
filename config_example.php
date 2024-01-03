c9887c3512076e677ada74b6478cd3047e6759ad
Loïc Faugières <loic.faugieres@gmail.com>
2024-01-03 17:59:11 +0100

    Centralisation (config.php) des colonnes d'authentification

--- config_example.php
+++ config_example.php
@@ -4,13 +4,23 @@ const ROUTE_PATH_PREFIX = '/website/';
 
 const DATABASE_CREDENTIALS = [
 
-    "dbms"          =>  "mysql",
+    "dbms"      =>  "mysql",
 
-    "host"          =>  "localhost",
-    "port"          =>  "3306",
-    "charset"       =>  "utf8mb4",
-    "name"          =>  "festiplan",
-    "user"          =>  "utilisateur_festiplan",
-    "password"      =>  "US3RfEs.T1PL4N."
+    "host"      =>  "localhost",
+    "port"      =>  "3306",
+    "charset"   =>  "utf8mb4",
+    "name"      =>  "festiplan",
+    "user"      =>  "utilisateur_festiplan",
+    "password"  =>  "US3RfEs.T1PL4N."
+
+];
+
+const AUTHENTIFICATION_COLUMNS = [
+
+    "table"     => "utilisateur",
+
+    "id"        => "id_utilisateur",
+    "login"     => "login_uti",
+    "password"  => "mdp_uti",
 
 ];
