52d45e83659d005e70f5e2600b00da7ae7eadf6f
Loïc Faugières <loic.faugieres@gmail.com>
2024-01-03 17:54:32 +0100

    Correction indentation exemple config

--- config_example.php
+++ config_example.php
@@ -4,13 +4,13 @@ const ROUTE_PATH_PREFIX = '/website/';
 
 const DATABASE_CREDENTIALS = [
 
-"dbms"          =>  "mysql",
+    "dbms"          =>  "mysql",
 
-"host"          =>  "localhost",
-"port"          =>  "3306",
-"charset"       =>  "utf8mb4",
-"name"          =>  "festiplan",
-"user"          =>  "utilisateur_festiplan",
-"password"      =>  "US3RfEs.T1PL4N."
+    "host"          =>  "localhost",
+    "port"          =>  "3306",
+    "charset"       =>  "utf8mb4",
+    "name"          =>  "festiplan",
+    "user"          =>  "utilisateur_festiplan",
+    "password"      =>  "US3RfEs.T1PL4N."
 
 ];
