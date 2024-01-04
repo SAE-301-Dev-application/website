fa9c5128607fbb899935b1ac287ce95ca748627d
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-01-04 16:46:24 +0100

    Création du composant de boîte d'aide de form

---
+++ src/Views/Components/FormHelpBox.php
@@ -0,0 +1,11 @@
+<div class="form-help-box">
+  <div class="box-header">
+    <i class="<?= $icon ?>"></i>
+
+    <h3 class="box-title">
+        <?= $title ?>
+    </h3>
+  </div>
+
+  <?= $content ?>
+</div>
