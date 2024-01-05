6c409980694bb6c827f280980981e1288d852baa
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-01-05 17:55:18 +0100

    Correction bug initialisation $files

--- src/Router/Engine/Request.php
+++ src/Router/Engine/Request.php
@@ -127,6 +127,8 @@ class Request
      */
     public function saveFiles(): array
     {
+        $this->files = [];
+        
         foreach ($_FILES as $fileKey => $file)
         {
             $this->files[$fileKey] = new File(
