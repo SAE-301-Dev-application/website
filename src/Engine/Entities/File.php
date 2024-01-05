5f206e0e3f83dff732ca7371e934186c96dc26d0
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-01-05 17:40:59 +0100

    Classes utilitaires d'entité File et Image créées
    Image étant une classe enfant à File.

---
+++ src/Engine/Entities/File.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace MvcLite\Engine\Entities;
+
+/**
+ * Class that represents a file.
+ *
+ * @author belicfr
+ */
+class File
+{
+    /** File name. */
+    private string $name;
+
+    /** File full path. */
+    private string $fullPath;
+
+    /** File type. */
+    private string $type;
+
+    /** File temporary name. */
+    private string $temporaryName;
+
+    /** File upload errors. */
+    private int $error;
+
+    /** File size. */
+    private int $size;
+
+    public function __construct(string $name,
+                                string $fullPath,
+                                string $type,
+                                string $temporaryName,
+                                int $error,
+                                int $size)
+    {
+        $this->name = $name;
+        $this->fullPath = $fullPath;
+        $this->type = $type;
+        $this->temporaryName = $temporaryName;
+        $this->error = $error;
+        $this->size = $size;
+    }
+
+    /**
+     * @return string File name
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string File full path
+     */
+    public function getFullPath(): string
+    {
+        return $this->fullPath;
+    }
+
+    /**
+     * @return string File type
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * @return string File temporary name
+     */
+    public function getTemporaryName(): string
+    {
+        return $this->temporaryName;
+    }
+
+    /**
+     * @return int File upload errors
+     */
+    public function getError(): int
+    {
+        return $this->error;
+    }
+
+    /**
+     * @return int File size
+     */
+    public function getSize(): int
+    {
+        return $this->size;
+    }
+
+    public function getContent(): string
+    {
+        return file_get_contents($this->getTemporaryName());
+    }
+
+    public function hasImage(): Image
+    {
+        return new Image(
+            $this->getName(), $this->getFullPath(), $this->getType(),
+            $this->getTemporaryName(), $this->getError(), $this->getSize()
+        );
+    }
+}
