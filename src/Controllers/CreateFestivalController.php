545d4a162bfef1db06a797f5898be764c96ca13a
Loïc Faugières <loic.faugieres@gmail.com>
2024-01-05 23:54:24 +0100

    Correction envoi erreurs dates + check nom festival déjà existant

--- src/Controllers/CreateFestivalController.php
+++ src/Controllers/CreateFestivalController.php
@@ -22,7 +22,7 @@ class CreateFestivalController extends Controller
         = "Le nom du festival ne doit pas dépasser 50 caractères.";
 
     private const ERROR_MAX_LENGTH_DESCRIPTION
-        = "La description du festival ne doit pas dépasser 1 000 caractères.";
+        = "La description du festival ne doit pas dépasser 1000 caractères.";
 
     private const ERROR_NAME_ALREADY_USED
         = "Ce nom de festival est déjà utilisé.";
@@ -91,12 +91,6 @@ class CreateFestivalController extends Controller
             ->maxLength("name", 50, self::ERROR_MAX_LENGTH_NAME)
             ->maxLength("description", 1000, self::ERROR_MAX_LENGTH_DESCRIPTION);
 
-        // if (Festival::hasFestival($request->getInput("name")))
-        // {
-        //     $validation->addError("nameAlreadyUsed", "name",
-        //         ERROR_NAME_ALREADY_USED);
-        // }
-
         if (count($checkedCategories) === 0)
         {
             $validation->addError("noCategorieChecked", "categories",
@@ -115,19 +109,21 @@ class CreateFestivalController extends Controller
                 ->dateSlot("beginning_date", "ending_date",
                     self::ERROR_ENDING_BEFORE_BEGINNING_DATE);
         }
-        else if (!$isBeginningDateRequired)
+        
+        if (!$isBeginningDateRequired)
         {
             $validation
                 ->futureDate("beginning_date", self::ERROR_START_DATE_IN_PAST);
         }
-        else
+        
+        if (!$isEndingDateRequired)
         {
             $validation
                 ->futureDate("ending_date", self::ERROR_END_DATE_IN_PAST);
         }
 
         // Get the file from the $_FILES superglobal
-        $imageFile = $request->getFile("illustration");
+        $imageFile = $request->getFile("illustration")->hasImage();
 
         // Check if there is a downloaded file
         if ($imageFile && $imageFile->getError() === UPLOAD_ERR_OK)
@@ -140,36 +136,25 @@ class CreateFestivalController extends Controller
                                       sprintf(self::ERROR_TYPE_NOT_SUPPORTED,
                                               '.' . $imageExtension));
             }
-            else
-            {
-                // Generate a unique file name
-                $imageName = uniqid("festival_") . "." . $imageExtension;
+        }
 
-                $imageFile->setName($imageName);
-
-                $pathToImageFolder = Storage::getResourcesPath()
-                                   . "/Medias/Images/FestivalsUploads/";
-
-                // Create the directory if it doesn't exist
-                if (!file_exists($pathToImageFolder)) {
-                    mkdir($pathToImageFolder, 0777, true);
-                }
-
-                $uploadPath = $pathToImageFolder . $imageName;
-                
-                // Create a file with the path $uploadPath
-                $newFile = fopen($uploadPath, "w") or die("Unable to open file!");
-
-                // Write the content of the uploaded file in the new file
-                fwrite($newFile, $imageFile->getContent());
-
-                // Close the new file
-                fclose($newFile);
-            }
+        if (!$validation->hasFailed()
+            && Festival::hasFestival($request->getInput("name")))
+        {
+            $validation->addError("nameAlreadyUsed", "name",
+                self::ERROR_NAME_ALREADY_USED);
         }
 
         if (!$validation->hasFailed())
         {
+            if ($imageFile && $imageFile->getError() === UPLOAD_ERR_OK)
+            {
+                // Generate a unique file name
+                $imageName = uniqid("festival_") . "." . $imageExtension;
+
+                $uploadPath = Storage::createImage($imageFile, "FestivalsUploads");
+            }
+
             Festival::create($request->getInput("name"),
                              $request->getInput("description"),
                              $imageName ?? null,
