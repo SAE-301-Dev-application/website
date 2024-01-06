b68736af9909ec145d2548c50744ec1a61584c91
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-01-06 01:16:23 +0100

    Classe parent Model créée

---
+++ src/Models/Engine/Model.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace MvcLite\Models\Engine;
+
+use MvcLite\Database\Engine\Database;
+
+class Model
+{
+    private string $tableName;
+
+    public function __construct(string $tableName)
+    {
+        $this->tableName = $tableName;
+    }
+
+    /**
+     * @return string Model table name
+     */
+    public function getTableName(): string
+    {
+        return $this->tableName;
+    }
+
+    /**
+     * @param string $tableName New model table name
+     * @return string New model table name
+     */
+    public function setTableName(string $tableName): string
+    {
+        return $this->tableName = $tableName;
+    }
+
+    /**
+     * @param string $column Column name
+     * @param string $value Value searched
+     * @return bool If value exists in given table column
+     */
+    public function isColumnValueExisting(string $column, string $value): bool
+    {
+        $query = "SELECT COUNT(*) as count FROM "
+               . $this->tableName
+               . " WHERE $column = ?";
+
+        $searching = Database::query($query, $value);
+        $result = $searching->get();
+
+        return $result["count"];
+    }
+}
