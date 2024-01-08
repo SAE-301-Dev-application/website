e285354d5a50adc33692e0e81b734a5a82008a15
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-01-08 11:44:10 +0100

    Dév. front end profil

--- src/Views/Profile.php
+++ src/Views/Profile.php
@@ -21,6 +21,10 @@ use MvcLite\Engine\Session\Session;
   <!-- JS -->
   <script src="/website/node_modules/jquery/dist/jquery.min.js" defer></script>
   <script src="/website/node_modules/gsap/dist/gsap.min.js" defer></script>
+  
+    <?php
+    Storage::include("Js/profile/navigation-links-manager.js", importMethod: "defer");
+    ?>
 
   <!-- FontAwesome -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -39,6 +43,189 @@ use MvcLite\Engine\Session\Session;
         <?= Session::getUserAccount()->getFirstname() ?>
         <?= Session::getUserAccount()->getLastname() ?>
       </h1>
+
+      <div class="main-grid">
+        <div class="left-side">
+          <div class="menu-container">
+            <a class="link active-link" href="#my_profile">
+              Mon profil
+            </a>
+
+            <a class="link" href="#my_festivals">
+              Mes festivals
+            </a>
+
+            <a class="link" href="#my_spectacles">
+              Mes spectacles
+            </a>
+
+            <a class="link" href="#settings">
+              Paramètres
+            </a>
+          </div>
+        </div>
+
+        <div class="main-side">
+          <section id="my_profile">
+            <h3 class="section-title">
+              Mon profil
+            </h3>
+
+            <form action="#" method="post">
+              <div class="form-component">
+                <label for="firstname">
+                  <p>
+                    Prénom :
+                  </p>
+
+                  <input type="text" name="firstname" id="firstname" />
+                </label>
+              </div>
+
+              <div class="form-component">
+                <label for="lastname">
+                  <p>
+                    Nom de famille :
+                  </p>
+
+                  <input type="text" name="lastname" id="lastname" />
+                </label>
+              </div>
+
+              <div class="form-component">
+                <label for="login">
+                  <p>
+                    Login :
+                  </p>
+
+                  <input type="text" name="login" id="login" />
+                </label>
+              </div>
+
+              <div class="form-component">
+                <label for="email">
+                  <p>
+                    Adresse e-mail :
+                  </p>
+
+                  <input type="email" name="email" id="email" />
+                </label>
+              </div>
+
+              <div class="form-buttons">
+                <button class="button-blue">
+                  <i class="fa-solid fa-save fa-xl"></i>
+                  Enregistrer
+                </button>
+
+                <button class="button-grey">
+                  <i class="fa-solid fa-eraser fa-xl"></i>
+                  Réinitialiser
+                </button>
+              </div>
+            </form>
+          </section>
+
+          <section id="my_festivals">
+            <h3 class="section-title">
+              Mes festivals
+            </h3>
+
+            <div class="festivals-container">
+              <div class="festival-preview">
+                <div class="festival-picture"></div>
+
+                <div class="festival-identity">
+                  <div class="festival-header">
+                    <div class="festival-name-container">
+                      <h3 class="festival-name">
+                        Festival name
+                      </h3>
+                      
+                      <i class="fa-solid fa-warning fa-2xl"></i>
+                    </div>
+
+                    <div class="festival-buttons-container">
+                      <button class="button-grey">
+                        <i class="fa-solid fa-pen"></i>
+                        Éditer
+                      </button>
+
+                      <button class="button-red">
+                        <i class="fa-solid fa-trash"></i>
+                        Supprimer
+                      </button>
+                    </div>
+                  </div>
+
+                  <p class="festival-description">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Consequuntur delectus minus officiis porro possimus.
+                    Ab at dignissimos eum harum in, ipsum, maxime porro possimus
+                    provident quam quasi quo sint ullam.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <section id="my_spectacles">
+            <h3 class="section-title">
+              Mes spectacles
+            </h3>
+
+            <div class="festivals-container">
+              <div class="festival-preview">
+                <div class="festival-picture"></div>
+
+                <div class="festival-identity">
+                  <div class="festival-header">
+                    <div class="festival-name-container">
+                      <h3 class="festival-name">
+                        Spectacle name
+                      </h3>
+
+                      <i class="fa-solid fa-warning fa-2xl"></i>
+                    </div>
+
+                    <div class="festival-buttons-container">
+                      <button class="button-grey">
+                        <i class="fa-solid fa-pen"></i>
+                        Éditer
+                      </button>
+
+                      <button class="button-red">
+                        <i class="fa-solid fa-trash"></i>
+                        Supprimer
+                      </button>
+                    </div>
+                  </div>
+
+                  <p class="festival-description">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Consequuntur delectus minus officiis porro possimus.
+                    Ab at dignissimos eum harum in, ipsum, maxime porro possimus
+                    provident quam quasi quo sint ullam.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <section id="settings">
+            <h3 class="section-title">
+              Paramètres
+            </h3>
+
+            <div class="buttons-container">
+              <button class="button-red">
+                Se désinscrire
+                <i class="fa-solid fa-right-from-bracket"></i>
+              </button>
+            </div>
+          </section>
+        </div>
+      </div>
     </div>
 
     <?php
