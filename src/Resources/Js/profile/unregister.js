3b8f105f74fcf6427f391292333cf2f1fe2a10b1
Loïc Faugières <loic.faugieres@gmail.com>
2024-01-09 23:21:46 +0100

    Renommage fonction js désinscription

--- src/Resources/Js/profile/unregister.js
+++ src/Resources/Js/profile/unregister.js
@@ -1,4 +1,4 @@
-const TOGGLING_POPUP_ELEMENTS = $('#unsubscribe_button, .popup > .popup-close-button'),
+const TOGGLING_POPUP_ELEMENTS = $('#unsubscribe_button, .popup > .popup-close-button, #popup_cancel_button'),
       CONFIRMATION_POPUP = $('#popup_confirm_account_deleting');
 
 TOGGLING_POPUP_ELEMENTS.on('click', () => {
