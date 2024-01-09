5e7b4986788099c25742c15256eb085f45f3f563
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-01-09 16:22:26 +0100

    Ajout attribut id

--- src/Views/Components/PopupComponent.php
+++ src/Views/Components/PopupComponent.php
@@ -1,9 +1,13 @@
-<div id="confirmation-popup" class="popup-container">
-    <div class="popup">
-        <h3 class="popup-title">
-            <?= $title ?>
-        </h3>
-
-        <?= $slot ?>
+<div class="popup-container popup-hidden" id="<?= $id ?>">
+  <div class="popup">
+    <div class="popup-close-button">
+      <i class="fa-solid fa-xmark"></i>
     </div>
+
+    <h3 class="popup-title">
+        <?= $title ?>
+    </h3>
+
+    <?= $slot ?>
+  </div>
 </div>
