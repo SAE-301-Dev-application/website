31ef814b0693aec6430a5037802f83073b8bae29
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-01-09 18:35:31 +0100

    Toggle + retrait logs

--- src/Resources/Js/profile/unsubscribe.js
+++ src/Resources/Js/profile/unsubscribe.js
@@ -1,9 +1,7 @@
 const TOGGLING_POPUP_ELEMENTS = $('#unsubscribe_button, .popup > .popup-close-button'),
       CONFIRMATION_POPUP = $('#popup_confirm_account_deleting');
 
-console.log("importé");
-
 TOGGLING_POPUP_ELEMENTS.on('click', () => {
-    console.log("cliqué", CONFIRMATION_POPUP);
+    $("body").toggleClass("body-overflow-clip");
     CONFIRMATION_POPUP.toggleClass("popup-hidden");
 });
