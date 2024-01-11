fda559cb2febe3aca2dde4c96b740b4bec1ae97c
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-01-11 11:54:27 +0100

    Vue NoJavaScript créée

---
+++ src/Views/NoJavaScript.php
@@ -0,0 +1,26 @@
+<?php
+use MvcLite\Engine\InternalResources\Storage;
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Activez JavaScript - Festiplan</title>
+
+  <!-- CSS -->
+  <?php
+  Storage::include("Css/ready.css");
+  ?>
+</head>
+<body>
+  <div id="nojs_main">
+    <h2>
+      Nous avons besoin de JavaScript poue fonctionner…
+    </h2>
+  </div>
+</body>
+</html>
