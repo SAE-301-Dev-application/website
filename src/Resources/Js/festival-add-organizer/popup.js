e2f90093e97b9fe786aed15550452bd2810a9e3f
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-01-14 13:31:42 +0100

    Script de la popup de recherche d'utilisateur créé

---
+++ src/Resources/Js/festival-add-organizer/popup.js
@@ -0,0 +1,7 @@
+const POPUP = $("#search_organizer_popup"),
+      POPUP_CLOSE_BUTTON = $(".popup > .popup-close-button");
+
+POPUP_CLOSE_BUTTON.on("click", () => {
+    POPUP.addClass("popup-hidden");
+    $("body").removeClass("body-overflow-clip");
+});
